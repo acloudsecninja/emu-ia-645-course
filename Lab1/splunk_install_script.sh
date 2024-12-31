5a3ef1301db00e2fdb44c1b6ccc511763656502d
acloudsecninja <rweber12@emich.edu>
2024-12-30 22:43:10 -0500

    typo

--- Lab1/splunk_install_script.sh
+++ Lab1/splunk_install_script.sh
@@ -6,9 +6,8 @@ docker pull splunk/splunk:latest ## This will pull the latest version of Splunk 
 ## This will deploy a newly created docker container with spunk with a default password of password ##
 ## and admin as the main user and it will allow the container to come up. ##
 ## This is for testing and dev environments only.
-docker run -d --name splunkfree --hostname splunkfree \
-  -p 8000:8000 -p 8088:8088 -p 514:514/udp -p 514:514 -p 9997:9997 \
-  -e SPLUNK_START_ARGS="--accept-license" \
-  -e SPLUNK_PASSWORD="password" \
-  -v ~/splunk-data:/opt/splunk/var \
-  splunk/splunk:latest
+docker run --name splunkfree --hostname splunkfree -p 8000:8000 -p 8080:8080 -p 514:514 -p 9997:9997  \
+  -e "SPLUNK_PASSWORD=password" \
+  -e "SPLUNK_START_ARGS=--accept-license" \
+  -e "SPLUNK_LICENSE_URI=Free" \
+  -it splunk/splunk:latest
